b2c0236746f9793bc78f331b92f6dd4269b5a8e8
spartak.gareginyan <spartak.gareginyan@iun.am>
2026-02-02 13:53:40 +0400

    while loop.

---
+++ src/main/kotlin/com/xcoder/loop/WhileLoop.kt
@@ -0,0 +1,11 @@
+package com.xcoder.loop
+
+fun main() {
+    val items = listOf("apple", "banana", "kiwifruit")
+    var index = 0
+
+    while (index < items.size) {
+        println("item at $index is ${items[index]}")
+        index++
+    }
+}
